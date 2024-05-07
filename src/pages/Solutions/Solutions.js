aa0c245471f146d62bdadbb20967a9a5c56d4ab7
Mark <datawithmark@gmail.com>
2024-05-07 17:39:26 +0530

    fix: DRO description

--- src/pages/Solutions/Solutions.js
+++ src/pages/Solutions/Solutions.js
@@ -19,7 +19,7 @@ function Solutions() {
       transition={{ duration: 1.5 }}
       className='card_grid'>
         <Card imgUrl={iva} title="Individual Voluntary Arrangements (IVA)" desc="In IVA all of your debt will be consolidated into one. You have to pay one single monthly payment which will cover all of your debts." solution="Formal Solution" />
-        <Card imgUrl={dro} title="Debt Relief Order (DRO)" desc="In DRO you have to pay only one time which is £90, and rest will be legally written off." solution="Formal Solution" />
+        <Card imgUrl={dro} title="Debt Relief Order (DRO)" desc="In DRO you don't need to pay anything and all of your debt will be legally written off." solution="Formal Solution" />
         <Card imgUrl={bankruptcy} title="Bankruptcy" desc="Bankruptcy debt solutions in the UK typically refer to legal processes that individuals or businesses may use when they are unable to repay their debts" solution="Formal Solution" />
         <Card imgUrl={dmp} title="Debt Management Plan (DMP)" desc="An informal agreement between you and your creditors for paying back your debts." solution="Informal Solution" />
         <Card imgUrl={td} title="Trust Deed" desc="A trust deed is a legally binding agreement between you and the people you owe money to, to pay all or some of the money you owe." solution="Scotish  Solution" />
